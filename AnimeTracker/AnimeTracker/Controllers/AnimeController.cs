e134f0047e1cd4d24bbc4adc62a0d88122c94f7b
MalikSh96 <malik96sharfo@hotmail.com>
2021-03-21 06:23:42 +0100

    code pushed up for the first time

---
+++ AnimeTracker/AnimeTracker/Controllers/AnimeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AnimeTracker.Models;
+
+namespace AnimeTracker.Controllers
+{
+    [Route("Anime")]
+    public class AnimeController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        [Route("")]
+        [Route("Anime")]
+        [Route("~/")]
+
+        public IActionResult Index()
+        {
+            ViewBag.Anime = db.Animes.ToList();
+            return View();
+        }
+    }
+}
---
+++ AnimeTracker/AnimeTracker/Models/Anime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimeTracker.Models
+{
+    [Table("Anime")]
+    public class Anime
+    {
+        [Key]
+        public int anime_id { get; set; }
+
+        public string animename { get; set; }
+        public string description { get; set; }
+        public string release_date { get; set; }
+
+    }
+}
